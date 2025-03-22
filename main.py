76aea89694ff7b150afb24916de70ac308f22a92
LuizVenosa <luizvenosa21@gmail.com>
2025-03-22 20:16:16 +0100

    camera movement estimator

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ from team_assginer import TeamAssigner
 from player_ball_assigner import PlayerBallAssigner
 import cv2
 import numpy as np
+from camera_movement_estimator import CameraMovementEstimator
+
+
+
+
+
 def main():
     videos_frames = read_video("input_videos/08fd33_4.mp4")
     
@@ -13,6 +19,13 @@ def main():
     tracks = tracker.get_object_tracks(videos_frames, read_from_stub=True, 
                                        stub_path="stubs/track_stubs.pkl")
 
+  
+    #Estimate Camera Movement
+    camera_movement_estimator = CameraMovementEstimator(videos_frames[0])
+    camera_movement_per_frame = camera_movement_estimator.get_camera_movement(videos_frames,
+                                                                               read_from_stub=True,
+                                                             stub_path="stubs/camera_movement.pkl")
+  
     #interploate ball positions
     tracks["ball"] = tracker.interpolate_ball_positions(tracks["ball"])
 
@@ -29,7 +42,7 @@ def main():
             team = teams_assigner.get_player_team(videos_frames[framen_num], track['bbox'], player_id)
             tracks['players'][framen_num][player_id]['team'] = team
             tracks['players'][framen_num][player_id]['team_color'] = teams_assigner.team_colors[team]
-         #Assign Ball to Player
+        #Assign Ball to Player
         ball_bbox = tracks["ball"][framen_num][1]["bbox"]
         assigned_player = player_assigner.assign_ball_to_player(player_track, ball_bbox)
 
@@ -43,6 +56,7 @@ def main():
    
     
     output_frames = tracker.draw_annotations(videos_frames, tracks)
+
     #calculate team ball control
     team_ball_control = np.array(team_ball_control)
     team_ball_control_team1 = np.sum(team_ball_control == 1)
@@ -51,10 +65,13 @@ def main():
     team_ball_control2 = team_ball_control_team2/(team_ball_control_team1 + team_ball_control_team2)
 
 
-
+    #Draw camera movement
+    output_video_frames = camera_movement_estimator.draw_camera_movement(output_frames, 
+                                                                         camera_movement_per_frame)
+    
 
     print("team ball control", team_ball_control1, team_ball_control2)
-    save_video(output_frames, "output_videos/output.avi")
+    save_video(output_video_frames, "output_videos/output.avi")
 
 
 
